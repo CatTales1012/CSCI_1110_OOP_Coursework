cb199d4147f7ae38828922200aff20561bb55114
404kevnotfound <student@HSiMac4-7.local>
2020-01-23 11:34:54 -0700

    Made program runable
    Fixed the class to fit better so that the program would run

--- Chpt 9/Rectangle9_1.java
+++ Chpt 9/Rectangle9_1.java
@@ -1,7 +1,8 @@
-class Rectangle9_1 {
+public class Rectangle9_1 {
 	public static void main(String[] args) {
 		//Rectangle 1 should have 40 height and 4 width
 		Rectangle rectangle1 = new Rectangle(40, 4);
+		System.out.println("For Rectangle 1:");
 		System.out.print("The width is " + rectangle1.width + ". ");
 		System.out.print("The height is " + rectangle1.height + ". ");
 		System.out.print("The area is " + rectangle1.getArea() + ". ");
@@ -9,35 +10,37 @@ class Rectangle9_1 {
 		
 		//Rectangle 2 should have 35.9 height and 3.5 width
 		Rectangle rectangle2 = new Rectangle(35.9, 3.5);
+		System.out.println("For Rectangle 2:");
 		System.out.print("The width is " + rectangle2.width + ". ");
 		System.out.print("The height is " + rectangle2.height + ". ");
 		System.out.print("The area is " + rectangle2.getArea() + ". ");
 		System.out.println("The perimeter is " + rectangle2.getPerimeter() + ". ");
 	}
-	class Rectangle{
-		double height = 1;
-		double width = 1;
-		
-		Rectangle(){
-			
-		}
-		
-		Rectangle(double h; double w;){
-			height = h;
-			width = w;
-		}
-		
-		double getArea(){
-			return width * height;
-		}
-		double getPerimeter(){
-			return (width * 2) + (height * 2);
-		}
-		void setHeight(double h){
-			height = h;
-		}
-		void setWidth(double w){
-			width = w;
-		}
+}
+class Rectangle{
+	double height;
+	double width;
+	
+	Rectangle(){
+		height = 1;
+		width = 1;
+	}
+	
+	Rectangle(double h , double w){
+		this.height = h;
+		this.width = w;
+	}
+	
+	double getArea(){
+		return width * height;
+	}
+	double getPerimeter(){
+		return (width * 2) + (height * 2);
+	}
+	void setHeight(double h){
+		height = h;
+	}
+	void setWidth(double w){
+		width = w;
 	}
 }
