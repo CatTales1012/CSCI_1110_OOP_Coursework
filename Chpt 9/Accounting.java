a8a79b7eba594f6a5460f88c10ce3e4bf3b98139
CatTales <student@HSiMac4-7.local>
2020-01-24 12:37:27 -0700

    Seperated Account and Accounting
    Rich said this would be easier for later.

--- Chpt 9/Accounting.java
+++ Chpt 9/Accounting.java
@@ -1,9 +1,9 @@
 /*
 Author: Cat Smith
 Assignment: 9-7, create Accounts.
-Date: January
+Date: January 24
 */
-class Accounting {
+public class Accounting {
 	public static void main(String[] args) {
 		/*
 		Account ID = 1122, Balance = 20,000, a.i.r. = 4.5%.
@@ -20,63 +20,13 @@ class Accounting {
 		
 		*/
 		Account account = new Account(1122, 20000);
-		account.withdraw(2500);  account.deposit(3000); account.setAnnualInterestRate((4.5 / 100)); 
+		account.withdraw(2500);  account.deposit(3000); account.setAnnualInterestRate((4.5 / 100)); account.getMonthlyInterest();
 		System.out.println("Account Information: ");
 		System.out.println("Account ID " + account.getId());
-		System.out.println("Balance: " + account.getBalance());
-		System.out.println("Monthly Interest Rate: " + account.getMonthlyInterestRate());
-		System.out.println("Account Start Date: " );
-	}
-}
-class Account {
-	private int id = 0;
-	private double balance = 0;
-	private double annualInterestRate = 0;
-	private double monthlyInterestRate = 0;
-	private java.util.Date dateCreated = new java.util.Date();
-	
-	Account(){
-		
-	}
-	Account(int i , double b){
-		id = i;
-		balance = b;
-	}
-	
-	int getId(){
-		return id;
-	}
-	void setId(int idSet){
-		id = idSet;
-	}
-	
-	double getBalance(){
-		return balance; 
-	}
-	void setBalance(double balSet){
-		balance = balSet;
-	}
-	
-	double getAnnualInterestRate(){
-		return annualInterestRate;
-	}
-	void setAnnualInterestRate(double airSet){
-		annualInterestRate = airSet;
-	}
-	
-	double getMonthlyInterestRate(){
-		monthlyInterestRate = annualInterestRate / 12;
-		return monthlyInterestRate;
-	}
-	double getMonthlyInterest(){
-		return balance * monthlyInterestRate;
-	}
-	
-	void withdraw(double withdrawValue){
-		balance = balance - withdrawValue;
-	}
-	
-	void deposit(double depositValue){
-		balance = balance + depositValue;
+		System.out.printf("Balance: $%.2f" , account.getBalance());
+		System.out.println();
+		System.out.println("Monthly Interest Rate: " + (account.getMonthlyInterestRate() * 100) + "%");
+		System.out.println("Annual Interest Rate: " + (account.getAnnualInterestRate() * 100) + "%");
+		System.out.println("Account Start Date: " + account.getDateCreated());
 	}
 }
