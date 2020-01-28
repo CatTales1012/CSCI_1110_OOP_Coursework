e8d241e3a1218d1ef064684705f4389428591142
CatTales <student@HSiMac4-76.local>
2020-01-28 12:04:49 -0700

    added two files
    added the two seperate files

---
+++ Chpt 10/10-3/Chpt10_3.java
@@ -0,0 +1,5 @@
+class Chpt10_3 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Chpt 10/10-3/MyInteger.java
@@ -0,0 +1,27 @@
+class MyInteger {
+	int value;
+	
+	MyInteger(int myValue){
+		
+	}
+	public int getInt(){
+		
+	}
+	public Boolean isEven(){}
+	public Boolean isEven(value){}
+	public Boolean isEven(MyInteger){}
+	
+	public Boolean isOdd(){}
+	public Boolean isOdd(value){}
+	public Boolean isOdd(MyInteger){}
+	
+	public Boolean isPrime(){}
+	public Boolean isPrime(value){}
+	public Boolean isPrime(MyInteger){}
+	
+	public Boolean equals(MyInteger , value){}
+	
+	public int parseInt(char[]){}
+	public int parseInt(String){}
+	
+}
