9cbdc463694146785ed375db0c6abd81647ec9b7
CatTales <student@HSiMac4-10.local>
2020-01-30 13:30:49 -0700

    Must fix the argument
    line 74 on MyInteger.java, must fix the argument to agree to convert to char[]

--- Chpt 10/10-3/Chpt10_3.java
+++ Chpt 10/10-3/Chpt10_3.java
@@ -1,5 +1,17 @@
+/*
+Author: Cat Smith
+Assignment: 10-3, create a MyInteger Class and create a client Program
+Date: January 30
+*/
 class Chpt10_3 {
 	public static void main(String[] args) {
-		
+		MyInteger aValue = new MyInteger(3);
+		if(aValue.isEven() == true){
+			System.out.println("A Value is Even");
+		}
+		else{
+			System.out.println("A Value is Odd");
+		}
+		System.out.print(aValue.getValue());
 	}
 }
--- Chpt 10/10-3/MyInteger.java
+++ Chpt 10/10-3/MyInteger.java
@@ -10,74 +10,78 @@ class MyInteger {
 	
 	public Boolean isEven(){
 		if(this.value % 2 == 0){
-			return True;
+			return true;
 		}
-		return False;
+		return false;
 	}
 	public static Boolean isEven(int value){
 		if(value % 2 == 0){
-			return True;
+			return true;
 		}
-		return False;
+		return false;
 	}
 	public static Boolean isEven(MyInteger value){
 		if(value.isEven()){
-			return True;
+			return true;
 		}
-		return False;
+		return false;
 	}
 	
 	public Boolean isOdd(){
 		if(this.value % 2 != 0){
-			return True;
+			return true;
 		}
-		return False;
+		return false;
 	}
 	public static Boolean isOdd(int value){
 		if(value % 2 != 0){
-			return True;
+			return true;
 		}
-		return False;
+		return false;
 	}
 	public static Boolean isOdd(MyInteger value){
 		if(value.isOdd()){
-			return True;
+			return true;
 		}
-		return False;
+		return false;
 	}
 	
 	public Boolean isPrime(){
-		for(int i = 2; i < this.value; i++){
-			if(this.value % i == 0){
-				return False;
-			}
-			return True;
-		}
+		return (isPrime(this.value));
 	}
 	public static Boolean isPrime(int value){
 		for(int i = 2; i < value; i++){
 			if(value % i == 0){
-				return False;
+				return false;
 			}
-			return True;
 		}
+		return true;
 	}
 	public static Boolean isPrime(MyInteger value){
 		if(value.isPrime()){
-			return True;
+			return true;
 		}
-		return False;
+		return false;
 	}
-	
+	public Boolean equals(int value){
+		return true;
+	}
 	public Boolean equals(MyInteger value){
-		
+		return true;
 	}
 	
-	public static int parseInt(char[]){
-		
+	public static int parseInt(char[] valueChar){
+		int out = 0;
+		for(int i = 0; i < valueChar.length; i++){
+			int temp = parseInt((valueChar[i]) - 48);
+			out = out * 10 + temp;
+		}
+		return out;
 	}
-	public static int parseInt(String){
-		
+	public static int parseInt(String value){
+		char[] aValue = value.toCharArray();
+		int something = parseInt(aValue);
+		return something;
 	}
 	
 }
