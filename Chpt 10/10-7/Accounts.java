0b88d6856a30fcfcc2cdf2fae74f66ec82552f4d
404kevnotfound <student@HSiMac4-7.local>
2020-01-31 13:16:51 -0700

    Created 10-7 assignment

---
+++ Chpt 10/10-7/Accounts.java
@@ -0,0 +1,61 @@
+/*
+Author: Cat Smith
+Assignment: 9-7, create Accounts.
+Date: January 31
+*/
+public class Account {
+	private int id = 0;
+		private double balance = 0;
+		private double annualInterestRate = 0;
+		private double monthlyInterestRate = 0;
+		private java.util.Date dateCreated = new java.util.Date();
+		
+		Account(){
+			
+		}
+		Account(int i , double b){
+			id = i;
+			balance = b;
+		}
+		
+		public int getId(){
+			return id;
+		}
+		public void setId(int idSet){
+			id = idSet;
+		}
+		
+		public double getBalance(){
+			return balance; 
+		}
+		public void setBalance(double balSet){
+			balance = balSet;
+		}
+		
+		public java.util.Date getDateCreated(){
+			return this.dateCreated;
+		}
+		public double getAnnualInterestRate(){
+			return annualInterestRate;
+		}
+		public void setAnnualInterestRate(double airSet){
+			annualInterestRate = airSet;
+		}
+		
+		public double getMonthlyInterestRate(){
+			monthlyInterestRate = annualInterestRate / 12;
+			return monthlyInterestRate;
+		}
+		public double getMonthlyInterest(){
+			return balance * monthlyInterestRate;
+		}
+		
+		public void withdraw(double withdrawValue){
+			balance = balance - withdrawValue;
+		}
+		
+		public void deposit(double depositValue){
+			balance = balance + depositValue;
+		}
+
+}
---
+++ Chpt 10/10-7/Atm.java
@@ -0,0 +1,46 @@
+/*
+Author: Cat Smith
+Assignment: 10-7, create an ATM for the 9-7 Accounting assignment
+Date: January 31
+*/
+import java.util.Scanner;
+public class Atm {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//These are the ID's 
+		int[] id = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		System.out.print("Enter an ID: ");
+		int check = input.nextInt();
+		
+		//Boolean to return true or false if it is valid 
+		Boolean isIn = false;
+		for(int i = 0; i < id.length; i++){
+			if(check == id[i]){
+				isIn = true;
+			}
+		}	
+		//while it is false, try again
+		while(isIn == false){
+			System.out.print("Please Enter a Valid Id: ");
+			check = input.nextInt();
+			
+			for(int i = 0; i < id.length; i++){
+				if(check == id[i]){
+					isIn = true;
+				}
+			}
+		}	
+		//if it is true, here are the menu options
+		if(isIn == true){
+			System.out.println("Main Menu");
+			System.out.println("1) Check Balance");
+			System.out.println("2) Withdraw");
+			System.out.println("3) Deposit");
+			System.out.println("4) Exit");
+		}
+		
+		
+		
+	}
+}
