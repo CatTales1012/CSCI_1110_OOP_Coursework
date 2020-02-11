67b5b5fe0bb2824e74184bf6cd991c4bbc59cedd
404kevnotfound <student@HSiMac4-7.local>
2020-02-11 12:23:20 -0700

    added the classes

---
+++ Chpt 11/11-1/Triangle.java
@@ -0,0 +1,37 @@
+class Triangle extends GeometricObject{
+	public double side1;
+	public double side2;
+	public double side3;
+	
+	public Triangle(){
+		
+	}
+	public Triangle(double side1, double side2, double side3){
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+	
+	public double getSide1(){
+		return side1;
+	}
+	public double getSide2(){
+		return side2;
+	}
+	public double getSide3(){
+		return side3;
+	}
+	
+	public double getArea(double area){
+		double s = (this.side1 + this.side2 + this.side3) / 2;
+		area = Math.sqrt(s((s - this.side1) (s - this.side2) (s - this.side3)));
+		return area;
+	}
+	public double getPerimeter(double perimeter){
+		perimeter = this.side1 + this.side2 + this.side3;
+		return perimeter;
+	}
+	public String toString(){
+		return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+	}
+}
