a429a8acc43d10227f43ca14ad6b3d49c7143229
CatTales <student@HSiMac4-30.local>
2020-02-12 13:32:23 -0700

    added this

---
+++ Chpt 11/11-3/Bank.java
@@ -0,0 +1,15 @@
+/*
+Author: Cat Smith
+Assignment: 11-3 create sub classes for savings and checkings
+Date: February 12
+*/
+import java.util.Scanner;
+public class Bank {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Savings save = new Savings(23, 100);
+		Checkings check = new Checkings(24, 100);
+		
+		
+	}
+}
